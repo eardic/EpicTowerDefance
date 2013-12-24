c21ce33e3863b9ee94a0ac5289f1f9e1d40529d1
Emre <Emre@Zangetsu>
2013-12-24 05:34:58 +0200

    Kule range düzenlendi.

--- src/gyte/cse443/etd/objects/TowerFactory.java
+++ src/gyte/cse443/etd/objects/TowerFactory.java
@@ -16,40 +16,42 @@ public class TowerFactory implements Factory {
 
     public Tower create(String type) {
         if (TowerType.CRYSTAL_TOWER.name().equals(type)) {
-            Tower t = new Tower(TowerType.CRYSTAL_TOWER,BulletType.RED,90, 200, 700);
+            Tower t = new Tower(TowerType.CRYSTAL_TOWER,BulletType.RED,110, 200, 700);
             t.loadGraphic(Resources.towerImg.replaceAll("#", "3"),false,false,32,64);
             return t;
         }
         if (TowerType.BLOOD_MAGIC.name().equals(type)) {
-            Tower t = new Tower(TowerType.BLOOD_MAGIC,BulletType.RED,80, 350, 600);
+            Tower t = new Tower(TowerType.BLOOD_MAGIC,BulletType.RED,100, 350, 600);
             t.loadGraphic(Resources.towerImg.replaceAll("#", "5"),false,false,32,64);
             return t;
         }
-        if (TowerType.CHINESE_TOWER.name().equals(type)) {
-            Tower t = new Tower(TowerType.CHINESE_TOWER,BulletType.GREEN,70, 400, 200);
-            t.loadGraphic(Resources.towerImg.replaceAll("#", "7"),false,false,32,64);
-            return t;
-        }
         if (TowerType.CANNON_TOWER.name().equals(type)) {
-            Tower t = new Tower(TowerType.CANNON_TOWER,BulletType.RED,60, 400, 300);
+            Tower t = new Tower(TowerType.CANNON_TOWER,BulletType.RED,100, 400, 300);
             t.loadGraphic(Resources.towerImg.replaceAll("#", "11"),false,false,32,64);
             return t;
+        }    
+        if (TowerType.WATER_MAGIC.name().equals(type)) {
+            Tower t = new Tower(TowerType.WATER_MAGIC,BulletType.BLUE,90, 300, 400);
+            t.loadGraphic(Resources.towerImg.replaceAll("#", "6"),false,false,32,64);
+            return t;
         }
+        
         if (TowerType.RIFLE_TOWER.name().equals(type)) {
-            Tower t = new Tower(TowerType.RIFLE_TOWER,BulletType.BLUE,50, 300, 200);
+            Tower t = new Tower(TowerType.RIFLE_TOWER,BulletType.BLUE,90, 300, 200);
             t.loadGraphic(Resources.towerImg.replaceAll("#", "12"),false,false,32,64);
             return t;
         }
         if (TowerType.GHOST_TOWER.name().equals(type)) {
-            Tower t = new Tower(TowerType.GHOST_TOWER,BulletType.GREEN,50, 300, 100);
+            Tower t = new Tower(TowerType.GHOST_TOWER,BulletType.GREEN,80, 300, 100);
             t.loadGraphic(Resources.towerImg.replaceAll("#", "10"),false,false,32,64);
             return t;
         }
-        if (TowerType.WATER_MAGIC.name().equals(type)) {
-            Tower t = new Tower(TowerType.WATER_MAGIC,BulletType.BLUE,50, 300, 400);
-            t.loadGraphic(Resources.towerImg.replaceAll("#", "6"),false,false,32,64);
+        if (TowerType.CHINESE_TOWER.name().equals(type)) {
+            Tower t = new Tower(TowerType.CHINESE_TOWER,BulletType.GREEN,80, 400, 200);
+            t.loadGraphic(Resources.towerImg.replaceAll("#", "7"),false,false,32,64);
             return t;
         }
+        
         
         return null;
     }
